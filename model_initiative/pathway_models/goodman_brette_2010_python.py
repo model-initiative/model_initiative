d33063d4d102726ca7f0d4fe2f47692361042ca4
jylls <jl10015@ic.ac.uk>
2017-11-14 17:02:56 +0000

    Update goodman_brette_2010_python.py

--- model_initiative/pathway_models/goodman_brette_2010_python.py
+++ model_initiative/pathway_models/goodman_brette_2010_python.py
@@ -8,16 +8,17 @@ def goodman_brette_2010_python(soundtest, number_neurons=20, noise=0.05):
     sound = Sound((soundtest[0], soundtest[1]))
     ## Create an hrtfset of number_neurons itds ##
     hset = HeadlessDatabase(itd=np.linspace(
-        0, 0.002, number_neurons), fractional_itds=False).load_subject()
+	0, 0.008, number_neurons), fractional_itds=False).load_subject()
     num_indices = hset.num_indices
-    hset_fb = hset.filterbank(Repeat(sound, num_indices))
+    swapped_channels = RestructureFilterbank(sound, indexmapping=[1, 0]) 
+    hset_fb = hset.filterbank(Repeat(swapped_channels, num_indices))
 
     ### Gammatone filtering ##
     gfb = Gammatone(hset_fb, tile(4 * kHz, hset_fb.nchannels))
 
     ###half wave rectification + compression ###
     cochlea = FunctionFilterbank(
-        gfb, lambda x: 15 * clip(x, 0, Inf)**(1.0 / 3.0))
+        gfb, lambda x: 5 * clip(x, 0, Inf)**(1.0 / 3.0))
 
     ### Leaky integrate and fire with noise ###
     eqs = '''
@@ -26,7 +27,7 @@ def goodman_brette_2010_python(soundtest, number_neurons=20, noise=0.05):
 	    '''
 
     G = FilterbankGroup(cochlea, 'I', eqs, threshold='V>1',
-                        reset='V=0', refractory=5 * ms)
+                        reset='V=0')
 
     ###Coincidence detector neurons ###
     cd = NeuronGroup(num_indices, eqs, threshold='V>1',
@@ -34,8 +35,8 @@ def goodman_brette_2010_python(soundtest, number_neurons=20, noise=0.05):
     C = Synapses(G, cd, 'we:1', pre='V += we')
     C.connect(True)
     for i in xrange(num_indices):
-        C.we[i, i] = 0.6
-        C.we[i + num_indices, i] = 0.6
+        C.we[i, i] = 0.5
+        C.we[i + num_indices, i] = 0.5
 
     # countercd=SpikeCounter(cd)
     spm = SpikeMonitor(cd)
