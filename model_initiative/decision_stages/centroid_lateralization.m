3853a5fb1cc7bd6e7931029de751eba2a5bfb9d7
jylls <jl10015@ic.ac.uk>
2018-01-10 14:01:35 +0000

    Update centroid_lateralization.m

--- model_initiative/decision_stages/centroid_lateralization.m
+++ model_initiative/decision_stages/centroid_lateralization.m
@@ -3,7 +3,7 @@ function response=centroid_lateralization(cross_correlogram,response_type_is_IID
 % cross_correlogram: Akeroyd Toolbox format
 % response_type: 0=ITD otherwise IID
 
-response=mccgramcentroid_x(cross_correlogram{1,1},0);       
+response=mccgramcentroid_BT(cross_correlogram{1,1},0);       
 
 if response_type_is_IID
     IIDScale=11.70;
